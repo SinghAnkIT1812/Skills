ebf72bdc214eb89b3524dbddd25faa4d1d0466e4
Ankit Kumar Singh <77728943+SinghAnkIT1812@users.noreply.github.com>
2021-06-11 18:22:02 +0530

    Update covid_data_tracker.py
    updae

--- covid_data_tracker.py
+++ covid_data_tracker.py
@@ -20,4 +20,28 @@ try:
     root.update()
     countries = data.get()
     country_names = countries.strip()
-    country_names =
+    country_names = country_names.replace(" ", ",")
+    country_names= country_names.split(",")
+    for x in country_names:
+        cases.append(covid.get_status_by_country_name(x))
+        root.update()
+    for y in cases:
+        confirmed.append(y["active"])
+        deaths.append(y["deaths"])
+        recoverd.append(y["recovered"])
+    confirmed_patch=mpatches.Patch(colour='green',label='confirmed')
+    recovered_patch=mpatches.Patch(colour='red',label='recovered')
+    active_patch=mpatches.Patch(color='blue',label='active')
+    deaths_patch=mpatches.Patch(color='black',label='deaths')
+    plt.legend(handles=[confirmed_patch,recovered_patch,active_patch,deaths_patch)
+    
+                        
+    for x in range(len(country_names)):
+                        
+                        plt.bar(country_names[x],confirmed[x],color='green')
+                        
+                        if recovered[x] > active[x]:
+                             plt.
+                        
+                    
+                        
